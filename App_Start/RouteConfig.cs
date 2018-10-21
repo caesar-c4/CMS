b132143ca1cee752c2aea022b7eedbdf0a0075c6
caesar-c4 <caesar13690@gmail.com>
2018-10-21 16:59:38 +0600

    Initial commit

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CMS
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Account", "Account/{action}/{id}", new { controller = "Account", action = "Index", id = UrlParameter.Optional }, new[] { "CMS.Controllers" });
+            
+            routes.MapRoute("Cart", "Cart/{action}/{id}", new { controller = "Cart", action = "Index", id = UrlParameter.Optional }, new[] { "CMS.Controllers" });
+
+            routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "CMS.Controllers" });
+
+            routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "CMS.Controllers" });
+
+            routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "CMS.Controllers" });
+
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "CMS.Controllers" });
+
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "CMS.Controllers" });
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+        }
+    }
+}
---
+++ Models/Data/CategoryDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CMS.Models.Data
+{
+    [Table("tblCatagories")]
+    public class CategoryDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int Sorting { get; set; }
+    }
+}
